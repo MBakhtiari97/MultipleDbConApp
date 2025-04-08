9d9cde212557619da5e082e3073e8c7e2ed7eb6e
Mohammad Bakhtiari <mohammadba86@yahoo.com>
2025-04-08 17:57:39 +0330

    Adding entities

---
+++ Core/Log.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Core;
+
+class Log
+{
+    [Key]
+    [Required]
+    public int LogId { get; set; }
+    [Required]
+    [StringLength(10, ErrorMessage = "{0} is too long")]
+    public string LogSerial { get; set; } = string.Empty;
+    [Required]
+    [StringLength(250, ErrorMessage = "{0} is too long")]
+    public string Description { get; set; } = string.Empty;
+    [Required]
+    public DateTime LogDateTime { get; set; } = DateTime.Now;
+    [Required]
+    public int UserId { get; set; }
+
+    #region Navigation Properties
+
+    public User User { get; set; } = null!;
+
+    #endregion
+}
---
+++ Core/User.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Core;
+
+class User
+{
+    [Key]
+    [Required]
+    public int UserId { get; set; }
+    [Required]
+    [StringLength(250, ErrorMessage = "{0} is too long")]
+    public string Username { get; set; } = string.Empty;
+    [Required]
+    [StringLength(250, ErrorMessage = "{0} is too long")]
+    [EmailAddress(ErrorMessage = "{0} is not valid")]
+    public string EmailAddress { get; set; } = string.Empty;
+    [Required]
+    [StringLength(20, ErrorMessage = "{0} is too long")]
+    public string Password { get; set; } = string.Empty;
+    [Required]
+    public DateTime RegisterDate { get; set; } = DateTime.Now;
+
+    #region Navigation Properties
+    public ICollection<Log> Logs { get; set; } = null!;
+    #endregion
+}
