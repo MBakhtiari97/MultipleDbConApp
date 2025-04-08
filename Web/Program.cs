7371fc91a72a66a2c63dc7cd0055c90726114684
Mohammad Bakhtiari <mohammadba86@yahoo.com>
2025-04-08 20:17:14 +0330

    Modifying program services

--- Web/Program.cs
+++ Web/Program.cs
@@ -1,21 +1,61 @@
+using DataLayer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration.UserSecrets;
+using Service.UserServices;
+
 namespace Web;
 
 public class Program
 {
+    private static string GetSecret()
+    {
+        var userSecretId = typeof(Web.Program).Assembly
+                        .GetCustomAttributes(typeof(UserSecretsIdAttribute), false)
+                        .OfType<UserSecretsIdAttribute>()
+                        .FirstOrDefault()?.UserSecretsId;
+        if (userSecretId == null)
+            throw new Exception("Init Application Configuration File Has Not Set Yet!");
+        return userSecretId!;
+    }
+
+    private static void AddConnectionStrings(WebApplicationBuilder builder)
+    {
+        var userSecretId = GetSecret();
+        builder.Configuration.AddUserSecrets(userSecretId);
+
+        var masterConnectionString = builder.Configuration.GetConnectionString("MasterConnection");
+        var logConnectionString = builder.Configuration.GetConnectionString("LogConnection");
+
+        builder.Services.AddDbContext<MasterDbContext>(options =>
+                                                         options.UseSqlServer(masterConnectionString)
+                                                         .EnableSensitiveDataLogging());
+        builder.Services.AddDbContext<LogDbContext>(options =>
+                                                 options.UseSqlServer(logConnectionString)
+                                                 .EnableSensitiveDataLogging());
+    }
+
+    private static void RegisterServices(WebApplicationBuilder builder)
+    {
+        builder.Services.AddScoped<IUserServices, UserServices>();
+        builder.Services.AddScoped<IUserReader, UserReader>();
+        builder.Services.AddScoped<IUserSaver, UserSaver>();
+    }
     public static void Main(string[] args)
     {
         var builder = WebApplication.CreateBuilder(args);
 
-        // Add services to the container.
+        AddConnectionStrings(builder);
+
+        RegisterServices(builder);
 
         builder.Services.AddControllers();
-        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
         builder.Services.AddEndpointsApiExplorer();
+
         builder.Services.AddSwaggerGen();
 
         var app = builder.Build();
 
-        // Configure the HTTP request pipeline.
         if (app.Environment.IsDevelopment())
         {
             app.UseSwagger();
@@ -25,7 +65,6 @@ public class Program
         app.UseHttpsRedirection();
 
         app.UseAuthorization();
-
 
         app.MapControllers();
 
