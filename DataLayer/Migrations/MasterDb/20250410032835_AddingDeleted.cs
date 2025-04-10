3cec7bc10ca0f45aa6d78bea5af30aeff367e9c9
Mohammad Bakhtiari <mohammadba86@yahoo.com>
2025-04-10 07:44:06 +0330

    Adding migrations

---
+++ DataLayer/Migrations/MasterDb/20250410032835_AddingDeleted.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DataLayer.Migrations.MasterDb
+{
+    /// <inheritdoc />
+    public partial class AddingDeleted : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "Deleted",
+                table: "AppUser",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Deleted",
+                table: "AppUser");
+        }
+    }
+}
